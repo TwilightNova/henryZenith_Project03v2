25590f171ae6faca396a25ab63de6274fb485c7d
Zenith <twilight.maelstrom.nova@gmail.com>
2022-12-07 22:28:14 -0600

    Ending Touches
    Final adjustments to spelling and code

--- Assets/_Game/Scripts/BGMManager.cs
+++ Assets/_Game/Scripts/BGMManager.cs
@@ -11,8 +11,10 @@ public class BGMManager : MonoBehaviour
     [SerializeField] private AudioClip clip;
 
     [Header("Audio Control")]
+    [Tooltip("Adjust the sound's volume, from muted to maximum volume.")]
     [Range(0, 1)]
     [SerializeField] private float _volume;
+    [Tooltip("Adjust the sound's pitch. This will speed up or slow down the audio.")]
     [Range(-3, 3)]
     [SerializeField] private float _pitch;
 
